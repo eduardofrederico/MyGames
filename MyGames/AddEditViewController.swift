9bbdcc1aad6fdbce0b3b3d6cf1d84a8be1a46cd3
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-08-12 22:46:43 -0300

    Atribuindo fotos aos nossos jogos.

--- MyGames/AddEditViewController.swift
+++ MyGames/AddEditViewController.swift
@@ -18,14 +18,70 @@ class AddEditViewController: UIViewController {
     @IBOutlet weak var ivCover: UIImageView!
 
     var game: Game!
+    lazy var pickerView: UIPickerView = {
+       let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.backgroundColor = .white
+        return pickerView
+    }()
+    
+    var consolesManager = ConsolesManager.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        toolbar.tintColor = UIColor(named: "main")
+        
+        let btCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let btDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        let btFlexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.items = [btCancel, btDone]
+        
+        tfConsole.inputView = pickerView
+        tfConsole.inputAccessoryView = toolbar
         
     }
     
+    @objc func cancel() {
+        tfConsole.resignFirstResponder()
+    }
+    
+    @objc func done() {
+        tfConsole.text = consolesManager.consoles[pickerView.selectedRow(inComponent: 0)].name
+        cancel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        consolesManager.loadConsoles(with: context)
+    }
+    
     @IBAction func addEditCover(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar Pôster", message: "De onde você quer escolher o pôster?", preferredStyle: .actionSheet)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default, handler: { (action: UIAlertAction) in self.selectPicture(sourceType: .camera)
+            })
+            alert.addAction(cameraAction)
+        }
+        let libraryAction = UIAlertAction(title: "Biblioteca de Fotos", style: .default) { (action: UIAlertAction) in self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        let photosAction = UIAlertAction(title: "Álbum de Fotos", style: .default) { (action: UIAlertAction) in self.selectPicture(sourceType: .savedPhotosAlbum)
+        }
+        alert.addAction(photosAction)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func selectPicture(sourceType: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        imagePicker.navigationBar.tintColor = UIColor(named: "main")
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func addEditGame(_ sender: UIButton) {
@@ -34,6 +90,11 @@ class AddEditViewController: UIViewController {
         }
         game.title = tfTitle.text
         game.releaseDate = dpReleaseDate.date
+        if !tfConsole.text!.isEmpty {
+            let console = consolesManager.consoles[pickerView.selectedRow(inComponent: 0)]
+            game.console = console
+        }
+        game.cover = ivCover.image
         
         do {
             try context.save()
@@ -53,4 +114,29 @@ class AddEditViewController: UIViewController {
     }
     */
 
+}
+
+
+extension AddEditViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return consolesManager.consoles.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    private func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> String? {
+        let console = consolesManager.consoles[row]
+        return console.name
+    }
+}
+
+extension AddEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage
+        ivCover.image = image
+        btCover.setTitle(nil, for: .normal)
+        dismiss(animated: true, completion: nil)
+    }
 }
